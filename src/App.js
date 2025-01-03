637a09df9e607a703fc6640ffff39bae1389be21
Alexander Heitman <akhbtclearn@gmail.com>
2025-01-03 14:37:20 -0700

    commiting

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 5);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Static React App</h1>
+        <p>Click the button below to increase the counter.</p>
+        <button onClick={incrementCounter}>Click Me</button>
+        <p>Button Clicked: <strong>{count}</strong> times</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
